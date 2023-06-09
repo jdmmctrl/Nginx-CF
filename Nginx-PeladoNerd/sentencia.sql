bae90b5b5af674e33b099738a14215d1c8f7b0d6
jdmmctrl <juandavidmurillomejia0718@outlook.es>
2023-06-08 22:03:14 -0500

    Actualizacion 08/06/2023/00

---
+++ Nginx-PeladoNerd/sentencia.sql
@@ -0,0 +1,3 @@
+GRANT ALL ON wordpress.* TO 'ctrladmin'@'localhost' IDENTIFIED BY 'ctrl' WITH GRANT OPTION;
+
+GRANT ALL ON wordpress.* TO 'ctrladmin'@'localhost' IDENTIFIED BY 'ctrl' WITH GRANT OPTION;
